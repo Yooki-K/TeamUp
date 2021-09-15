08e8c33aae39c6e05a5f62309854e931b8e5b3aa
McFly <mcfly@njust.edu.cn>
2021-09-15 22:51:20 +0800

    RoomInf.java

---
+++ src/main/java/com/teamup/demo/roomManage/entity/RoomInf.java
@@ -0,0 +1,37 @@
+package main.java.com.teamup.demo.roomManage.entity;
+
+public class RoomInf {
+    private int id;
+    private int roomId;
+    private String user;
+
+    public RoomInf(int id, int roomId, String user) {
+        this.id = id;
+        this.roomId = roomId;
+        this.user = user;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(int roomId) {
+        this.roomId = roomId;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+}
