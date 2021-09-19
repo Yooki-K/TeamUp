fc3d003b19b7982121a0682b2377339f23fe7f11
McFly <mcfly@njust.edu.cn>
2021-09-19 17:46:30 +0800

    文件整合

--- src/test/java/com/teamup/demo/DemoApplicationTests.java
+++ src/test/java/com/teamup/demo/DemoApplicationTests.java
@@ -1,10 +1,10 @@
 package com.teamup.demo;
 
-import com.teamup.demo.userManage.service.CodeService;
+import com.teamup.demo.tool.SendMail;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import javax.annotation.Resource;
+import java.util.Date;
 
 @SpringBootTest
 class DemoApplicationTests {
@@ -13,22 +13,9 @@ class DemoApplicationTests {
 //    void myTest() {
 //        SendMail.sendSimpleMail("ky_yans@163.com");
 //    }
-//    void myTest(){
-//
-//        long t = codeService.deleteInvalid(new Date().getTime());
-//        System.out.println(t);
-//    }
-//void myTest(){
-//    Custom cu = new Custom();
-//    System.out.println(custom.toString());
-//    System.out.println(cu.toString());
-////        Custom{labels=[擅长前端, 擅长后端, 擅长报告, 擅长数据库], schools=[南京理工大学, 南京大学]}
-////        Custom{labels=null, schools=null}
-//
-//}
-    @Resource
-    CodeService codeService;
     @Test
     void myTest(){
+        long time = new Date().getTime();
+        System.out.println(time);
     }
 }
